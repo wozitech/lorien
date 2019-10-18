224411bb424c87c52cc759fd41d95952be8b15d9
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-10-18 11:27:22 +0100

    removing unnecessary "local" function in createOrganisation

--- src/local/createOrganisation.js
+++ src/local/createOrganisation.js
@@ -35,30 +35,6 @@ const thisEvent = {
     body: JSON.stringify(organisation),
 };
 
-
-const local = async () => {
-    try {
-        console.log('Create organisation: ', organisation.name);
-        const returnVal = await myOrg.handler(
-            thisEvent,
-            { invokedFunctionArn : 'arn:aws:lambda:eu-west-1:accountid:function:wozitech-tech-test-lambda' },
-            (err, data) => {
-                if (err) {
-                    console.error(err);
-                    return -1;
-                }
-
-                console.log("local: My results: ", data);
-                return 1;
-            }
-        );
-        console.log("myOrg.handler returned: ", returnVal);
-        
-    } catch (err) {
-        console.error("Caught local error: ", err);
-    }
-}
-
 process.env.LOG_LEVEL = 3;
 ;(async () => {
     try {
